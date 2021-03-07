db82696790e3077b5022eec9e6db1e6bd864ddbc
adamglw <53101365+adamglw@users.noreply.github.com>
2021-03-07 22:58:25 +0000

    Add statusbar for m1.

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,23 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+  /*Icon*/    /*Command*/                  /*Update Interval*/  /*Update Signal*/
+  {" ",        "~/.local/bin/kernel",      3600,                0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+  {" ",        "~/.local/bin/pacupdate",   3600,                0},
+
+  {" ",       "~/.local/bin/cpupercent",   3,                   0},
+
+  {" ",        "~/.local/bin/cputemp",     3,                   0},
+
+  {" ",       "~/.local/bin/mem",          40,                  0},
+
+  {" ",       "~/.local/bin/volume",       0,                   10},
+
+  {" ",       "~/.local/bin/upt",          60,                  0},
+
+  {" ",       "~/.local/bin/dte",          10,                  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 10 ;
