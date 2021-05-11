af461321be8d2d92435f238d5eb1a6584ca421a5
adamglw <53101365+adamglw@users.noreply.github.com>
2021-05-12 00:14:23 +0100

    Remove vertical bars

--- blocks.def.h
+++ blocks.def.h
@@ -7,17 +7,17 @@ static const Block blocks[] = {
 
 //  {"",       "~/.local/bin/upt",         60,                  0},
   
-  {"  ",     "~/.local/bin/pacupdate",    3600,                0},
+  {"  ",       "~/.local/bin/volume",       0,                   10},
+
+  {"",       "~/.local/bin/pacupdate",    3600,                0},
   
   {"",       "~/.local/bin/cpupercent",   3,                   0},
   
   {"",       "~/.local/bin/mem",          40,                  0},
 
-  {"",       "~/.local/bin/volume",       0,                   10},
-
   {"",       "~/.local/bin/dte",          10,                  0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  |  ";
+static char delim[] = "    ";
 static unsigned int delimLen = 10 ;
